af62e3e577a59e25887ed6d90851f456c492cede
William Knowles <will@hollyandwill.com>
2016-03-13 19:50:00 +0000

    projection changes and styling

---
+++ projections/natural-earth/min/script-min.js
@@ -0,0 +1,41 @@
+var width = 960,
+    height = 500;
+
+var projection = d3.geo.naturalEarth()
+    .scale(167)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("body #map-wrapper").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.append("defs").append("path")
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
+
+svg.append("use")
+    .attr("class", "fill")
+    .attr("xlink:href", "#sphere");
+
+d3.json("world-50m.json", function(error, world) {
+  if (error) throw error;
+
+  svg.insert("path")
+      .datum(topojson.feature(world, world.objects.land))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.insert("path")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+});
+
+d3.select(self.frameElement).style("height", height + "px");
+
+
